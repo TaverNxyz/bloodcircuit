b6964ee50b64188e3eb8ec684f8ccf1fc2a04e16
Ξsco <voee178@yahoo.com>
2025-01-04 05:37:04 -0800

    Delete src/integrations/supabase directory

--- src/integrations/supabase/types/index.ts
+++
@@ -1,5 +0,0 @@
-export type { Database } from './database';
-export type { ProfilesTable, Profile } from './profiles';
-export type { OrdersTable, Order } from './orders';
-export type { SubscriptionsTable, Subscription } from './subscriptions';
-export type { VouchesTable, Vouch, VouchWithProfile } from './vouches';
